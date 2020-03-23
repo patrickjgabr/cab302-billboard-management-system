b900f5c920789392ae6d2e80ba65c3bf761aa2c8
Synntech <patrickjgabr@gmail.com>
2020-03-24 08:13:39 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# cab302-billboard-management-system
---
+++ src/ControlPanel/ControlPanel.java
@@ -0,0 +1,7 @@
+package ControlPanel;
+
+public class ControlPanel {
+    public static  void main (String[] args) {
+        System.out.println("Hello World from Control Panel!");
+    }
+}
