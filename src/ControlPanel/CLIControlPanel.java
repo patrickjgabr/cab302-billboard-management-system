47983104673e369090d4f11c96121794478cebc5
Synntech <patrickjgabr@gmail.com>
2020-04-01 10:11:34 +1000

    Create message class.

---
+++ src/ControlPanel/CLIControlPanel.java
@@ -0,0 +1,35 @@
+package ControlPanel;
+
+import java.util.Scanner;
+
+public class ControlPanel {
+    public static  void main (String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Hello World from Control Panel!");
+        System.out.print("Username: ");
+        String username = keyboard.nextLine();
+        System.out.print("Password: ");
+        String password = keyboard.nextLine();
+        System.out.println("CLI Menu (Administrator):");
+        System.out.println("    1. Billboards");
+        System.out.println("    2. Scheduler");
+        System.out.println("    3. User Management");
+        int selection = keyboard.nextInt();
+        System.out.print("Option: ");
+        if (selection == 1) {
+            System.out.println("Billboards");
+            System.out.println("    1. Create Billboard");
+            System.out.println("    2. Import Billboard");
+            System.out.println("    2. Show Billboards");
+        }
+        else if (selection == 2) {
+            System.out.println("Scheduler");
+        }
+        else if (selection == 3) {
+            System.out.println("User Management");
+            System.out.println("    1. Create User");
+            System.out.println("    2. Show Users");
+        }
+
+    }
+}
