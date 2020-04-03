68a76bd4b1c00d0b2b4a8b29503b1623960f753a
harry <harryweyand76@gmail.com>
2020-04-03 11:46:25 +1000

    Added font class, removed BillboardGUI class
    ControlPanel.java now renders GUI. Separate class for each tab. added display showing selected row.

--- src/ControlPanel/BillboardTab.java
+++ src/ControlPanel/BillboardTab.java
@@ -1,22 +1,24 @@
 package ControlPanel;
 
 import Shared.Billboard;
-
+import static ControlPanel.CustomFont.*;
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.util.ArrayList;
 
-public class BillboardTab {
-    public static JPanel SetupBillboardsPane(JPanel pane) {
-        JPanel panel1 = new JPanel();                           //first tab
-        panel1.setBorder(BorderFactory.createEmptyBorder(30,20,10,20));
+public class BillboardTab{
+    public static JPanel SetupBillboardsPane() {
+        JPanel panel1 = new JPanel();                                                           //first tab
+        panel1.setBorder(BorderFactory.createEmptyBorder(30,20,15,20));
         panel1.setLayout(new GridLayout(2,1));
-        panel1.setBackground(new Color(102,102,102));
+        panel1.setBackground(lightGray);
         return panel1;
     }
 
-    public static JPanel SetupBillboardsTable(JPanel pane, ArrayList<Billboard> billboards) {
-        String[][] tableContents = new String[billboards.size()][9];
+    public static void SetupBillboardsTable(JPanel pane, ArrayList<Billboard> billboards) {
+        String[][] tableContents = new String[billboards.size()][9];                            //populating table contents with billboard array
         Integer i = 0;
         for (Billboard billboard : billboards) {
             tableContents[i][0] = i.toString();
@@ -30,16 +32,55 @@ public class BillboardTab {
             tableContents[i][8] = billboard.getInformationTextColour();
             i++;
         }
-        String[] columns = {"ID","Billboard","Author", "IMG SRC","Message", "Msg Colour", "BG Colour", "Info Text", "Info Colour"};
+        String[] columns = {"ID","Billboard","Author", "IMG SRC","Message", "Msg Colour", "BG Colour", "Info Text", "Info Colour"};     //Column headers
+
         JTable table = new JTable(tableContents, columns);
+        setTableFeatures(table);                            //set table font, layout, size, colour etc.
+        pane.add(new JScrollPane(table));                   //add table to pane - 1st row out of 2 in the grid layout.
+        //------------------------------------Table Created --------------------------------------------------------//
+        JLabel bottomGrid = new JLabel();
+        bottomGrid.setLayout(new GridLayout(2,3));       //2nd row of pane gridLayout contains a label with 2 rows 3 cols
+
+        JButton previewButton = new JButton("Preview");                    //button to be placed at grid space (0,1)
+        JButton createButton = new JButton("Create New");                  //button to be placed at grid space (2,1)
+        setButtonLook(previewButton);
+        setButtonLook(createButton);
+
+        JLabel selectedRow = new JLabel();
+        selectedRow.setFont(tableContentsF);
+
+        ListSelectionModel rowSelected = table.getSelectionModel();             //setup list selection model to listen for a selection of the table
+        rowSelected.addListSelectionListener(e -> {
+            if (!rowSelected.isSelectionEmpty()){
+                int selected = rowSelected.getMinSelectionIndex();
+                selectedRow.setText("Row "+selected+" Selected");               //change label text to display selected row.
+            }
+        });
+
+        bottomGrid.add(selectedRow);        //add label showing which row is selected
+        bottomGrid.add(new JLabel());       //add 2 blank labels in grid locations (1,0) and (2,0) - room to replace in future
+        bottomGrid.add(new JLabel());
+        bottomGrid.add(previewButton);                 //place button 1 at (0,1)
+        bottomGrid.add(createButton);                 //place button 2 at (0,2)
+        pane.add(bottomGrid);
+    }
+
+
+    public static void setButtonLook(JButton b){
+        b.setBorder(BorderFactory.createLineBorder(Color.black, 3));        //set button border, font, colours etc.
+        b.setFont(buttons);
+        b.setForeground(softBlue);
+    }
+    public static void setTableFeatures(JTable table){
         table.setRowHeight(40);
-        table.setIntercellSpacing(new Dimension(10, 20));        //##
-        //table.setFont(tableContentsF);
-        //table.getTableHeader().setBackground(Color.GRAY);
+        table.setRowSelectionAllowed(true);
+        table.setColumnSelectionAllowed(true);
+        table.getColumnModel().getColumn(0).setMaxWidth(35);    //set column 0 to max 35 wide (doesn't need to be big)
+        table.setIntercellSpacing(new Dimension(10, 20));
+        table.setFont(tableContentsF);                                      //table contents font (16px Comic sans)
+        table.getTableHeader().setBackground(softBlue);                     //set table header colour
         table.getTableHeader().setOpaque(false);
-        //table.getTableHeader().setFont(tableHeader);
-        table.setEnabled(false);                //uneditable
-        pane.add(new JScrollPane(table));
-        return pane;
+        table.getTableHeader().setFont(new Font("Comic Sans", Font.ITALIC, 18));
+        table.setDefaultEditor(Object.class, null);
     }
 }
--- src/ControlPanel/ControlPanel.java
+++ src/ControlPanel/ControlPanel.java
@@ -2,19 +2,20 @@ package ControlPanel;
 
 import Shared.Billboard;
 import Shared.TestCase;
-
+import static ControlPanel.CustomFont.*;
 import javax.swing.*;
 import java.util.ArrayList;
 
 public class ControlPanel {
     public static void main(String[] args) {
-        ArrayList<Billboard> billboards = TestCase.Billboards(); //load test billboards
-        JFrame frame = GUI.SetupFrame(); //init gui
-        JTabbedPane pane = new JTabbedPane(); //init tabs
-        JPanel billboardsPane = new JPanel();; //init billboard pane
-        JPanel schedulePane = new JPanel();; //init schedule pane
-        JPanel userManagementPane = new JPanel();; //init userManagement pane
-        billboardsPane = BillboardTab.SetupBillboardsPane(billboardsPane);
+        ArrayList<Billboard> billboards = TestCase.Billboards();        //load test billboards
+        JFrame frame = GUI.SetupFrame();                                //init gui
+        JTabbedPane pane = new JTabbedPane();                           //init tabs
+        pane.setFont(tabs);
+        JPanel billboardsPane = new JPanel();                           //init billboard pane
+        JPanel schedulePane = new JPanel();                             //init schedule pane
+        JPanel userManagementPane = new JPanel();                       //init userManagement pane
+        billboardsPane = BillboardTab.SetupBillboardsPane();
         BillboardTab.SetupBillboardsTable(billboardsPane, billboards);
         pane.addTab("Billboards", billboardsPane);
         pane.addTab("Schedule", schedulePane);
