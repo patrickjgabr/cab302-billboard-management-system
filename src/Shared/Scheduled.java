d33263f5e6b5cf0c55d87af030b086cba4a2e29d
Synntech <patrickjgabr@gmail.com>
2020-04-05 15:50:35 +1000

    Scheduled getters.

--- src/Shared/Scheduled.java
+++ src/Shared/Scheduled.java
@@ -1,4 +1,47 @@
 package Shared;
 
+import java.awt.image.BufferedImage;
+import java.util.Calendar;
+
 public class Scheduled {
+    private int id;
+    private String name;
+    private String creator;
+    private Calendar startTime;
+    private Calendar endTime;
+    private int duration;
+    //private BufferedImage preview; Preview image later
+
+    public Scheduled(int id, String name, String creator, Calendar startTime, Calendar endTime, int duration){
+        this.id = id;
+        this.name = name;
+        this.creator = creator;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.duration = duration;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public Calendar getStartTime() {
+        return startTime;
+    }
+
+    public Calendar getEndTime() {
+        return endTime;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
 }
