3f6ce025d634f54858bdd179256d8042c801d32d
Synntech <patrickjgabr@gmail.com>
2020-05-18 13:33:00 +1000

    Create stub functions for TDD.

--- src/Shared/ScheduleHelper.java
+++ src/Shared/ScheduleHelper.java
@@ -1,4 +1,16 @@
 package Shared;
 
 public class ScheduleHelper {
+    public static Calendar DateTime (int day, int hour, int minute, int period) {
+
+    }
+
+    public static Calendar minutes (int minutes) {
+
+    }
+
+    public static ArrayList<Event> GenerateEvents (ArrayList<Scheduled> schedule) {
+
+
+    }
 }
